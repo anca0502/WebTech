4e8c3065bc1658ece20ae23122759d3011eef689
anca0502 <your-email@domain.com>
2025-10-19 16:48:48 +0000

    for + ...num param

---
+++ sem2/lstToArr.js
@@ -0,0 +1,16 @@
+// function occurences(text, character) {
+//   let count = 0;
+//   for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === character) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// console.log(occurences("sample text", "e"));
+
+function lstToArr(...numere) {
+  return numere;
+}
+console.log(lstToArr(1, 2, 3, 4, 5, 6, 7));
