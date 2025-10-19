5854f2b5db24857e0b854cd3494d0650391a92ba
anca0502 <your-email@domain.com>
2025-10-19 15:33:05 +0000

    ping pong

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-var http = require("http");
+// var http = require("http");
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// //create a server object:
+// http
+//   .createServer(function (req, res) {
+//     res.write("Hello from CodeSandbox!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("HeLLoWorld!");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(8080);
